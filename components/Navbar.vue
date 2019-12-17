d7050f31ea9182d2b474092763b14a1501248ddc
Nico De Witte <nico.dewitte@outlook.com>
2019-12-17 13:55:09 +0100

    Add navbar from devbit course made by Sille

---
+++ components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-app-bar
+      color="red darken-2"
+      dark
+    >
+      <v-toolbar-title>{{ $site.title }}</v-toolbar-title>
+
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted: function() {
+    // TODO: add this.$site.themeConfig.nav links to navbar
+    // console.log(this.$site)
+  }
+}
+</script>
--- layouts/Layout.vue
+++ layouts/Layout.vue
@@ -1,5 +1,33 @@
 <template>
-  <div class="theme-container">
-    <Content/>
-  </div>
+  <v-app light>
+    <v-content>
+      <NavBar/>
+      <v-container>
+        <v-row justify="center">
+          <v-col sm="4" lg="2">
+            <v-img src="/images/logo.png" alt="DevBit logo" contain />
+          </v-col>
+        </v-row>
+        <Content />
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
+
+<script>
+
+import NavBar from '@theme/components/Navbar.vue'
+
+export default {
+  name: 'layout',
+  components: {
+    NavBar
+  },
+}
+</script>
+
+<style>
+  .header-anchor {
+    display: none;
+  }
+</style>
